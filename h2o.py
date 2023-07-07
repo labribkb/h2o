1239fdb90ea355e446ef1e2c04bcffd318d41ec6
Pomme-Cassierou Luc <luc.pomme-cassierou@u-bordeaux.fr>
2023-07-07 17:14:32 +0200

    License update

--- h2o.py
+++ h2o.py
@@ -2,14 +2,24 @@
 method called H^2O.
 """
 
-__author__ = "Luc Etienne Pommé"
+__author__ = ["Luc Etienne Pommé", "Romain Bourqui"]
 __copyright__ = "Copyright (C) Univ. de Bordeaux"
 __credits__ = ["Luc Etienne Pommé", "Romain Bourqui", "Romain Giot"]
 
-__licence__ = "GPL"
+__license__ = "GNU General Public License, version 3"
 __version__ = "1.0"
 __maintainer__ = "Luc Etienne Pommé"
 __email__ = "luc.pomme-cassierou@u-bordeaux.fr"
+
+"""
+ This file is part of H2O.
+
+H2O is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import gc
 import itertools
